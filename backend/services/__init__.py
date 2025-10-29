c545bb4280272ccdcb1e66da4b87199eb112c3c8
Chirag Sawant <sawantch099@gmail.com>
2025-10-29 19:10:09 +0530

    New implementation
    New implementation

--- backend/services/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Database and external services."""
-
-from .pinecone_service import PineconeService
-from .mongodb_service import MongoDBService
-
-__all__ = ["PineconeService", "MongoDBService"]
